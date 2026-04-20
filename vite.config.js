f88fe7b0ff362e7993c247ec9e94992a4e912fe4
MahmoudMuhhamed <mahmoudzaraa10@email.com>
2026-04-21 01:28:06 +0200

    update deploy pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: '/Social-App/',
+  base: '/',
 });
